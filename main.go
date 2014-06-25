6bf9461d629ba48e83bfb553dddb1653e3a01096
Nick Robinson <nr33@msstate.edu>
2014-06-24 21:05:19 -0500

    Using more mainstream sqlite3 library

--- main.go
+++ main.go
@@ -1,48 +1,69 @@
 package main
 
 import (
-	"code.google.com/p/go-sqlite/go1/sqlite3"
-	"crypto/md5"
-	"database/sql"
-	"fmt"
-	"io"
-	"log"
+    "database/sql"
+    "fmt"
+    "github.com/mattn/go-sqlite3"
+    "log"
+    "os"
 )
 
 func main() {
-	db, _ := sql.Open("sqlite3", "sqlite.db")
-	h := md5.New()
-	defer db.Close()
-	db.Exec("CREATE TABLE users(a PRIMARY KEY, b, c)")
-	io.WriteString(h, "password")
-	args := sqlite3.NamedArgs{"$a": "admin", "$b": "Nick Robinson", "$c": h.Sum(nil)}
-	db.Exec("REPLACE INTO users VALUES($a, $b, $c)", args)
-	fmt.Printf(">")
-	for {
-		var str, realname, username, password string
-		fmt.Scanf("%s", &str)
-		switch str {
-		default:
-			fmt.Printf("echo %s\n", str)
-			fmt.Printf(">")
-		case "exit":
-			return
-		case "enable":
-			fmt.Printf("Username: ")
-			fmt.Scanf("%s", &username)
-			fmt.Printf("Password: ")
-			fmt.Scanf("%s", &password)
-			err := db.QueryRow("SELECT b FROM users WHERE a=? AND c=?", username, password).Scan(&realname)
-			switch {
-			case err == sql.ErrNoRows:
-				log.Printf("No user with that ID.")
-				fmt.Printf(">")
-			case err != nil:
-				log.Fatal(err)
-			default:
-				fmt.Printf("Username is %s\n", realname)
-				fmt.Printf(">")
-			}
-		}
-	}
+    //Remove current db file
+    os.Remove("./sqlite.db")
+
+    db, err := sql.Open("sqlite3", "./sqlite.db")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    sql := `
+		CREATE TABLE users(username PRIMARY KEY, firstlast, password);
+	`
+    _, err = db.Exec(sql)
+    if err != nil {
+        log.Printf("%q: %s\n", err, sql)
+        return
+    }
+
+    tx, err := db.Begin()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    stmt, err := tx.Prepare("insert into users(username, firstlast, password) values(?, ?, ?)")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer stmt.Close()
+
+    _, err = stmt.Exec("admin", "Nick Robinson", "password")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf(">")
+    for {
+        var str, realname, username, password string
+        fmt.Scanf("%s", &str)
+        switch str {
+        default:
+            fmt.Printf("echo %s\n", str)
+            fmt.Printf(">")
+        case "exit":
+            return
+        case "enable":
+            fmt.Printf("Username: ")
+            fmt.Scanf("%s", &username)
+            fmt.Printf("Password: ")
+            fmt.Scanf("%s", &password)
+            err := db.QueryRow("SELECT b FROM users WHERE a=? AND c=?", username, password).Scan(&realname)
+            if err != nil {
+                log.Fatal(err)
+            }
+            fmt.Println(realname)
+        }
+    }
 }
